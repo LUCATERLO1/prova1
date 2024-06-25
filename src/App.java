50c1f41bae68288998fab52659b0b75ee60e14e8
LUCATERLO1 <lukran32@gmail.com>
2024-06-25 15:20:13 +0200

    primo commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        es10();
+    }
+
+    public static void es10() {
+        System.out.println("Ciaoo Mondo");
+    }
+}
